bdf343035156551a48b4c5a5bd898d3307d1c5c0
dipu-37 <razibul1105nstu@gmail.com>
2024-06-27 10:38:20 +0700

    smart home starter just template setup

---
+++ src/main.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from './components/ErrorPage.jsx';
+import Home from './components/Home.jsx';
+import Root from './components/Root.jsx';
+import About from './components/About.jsx';
+import Shop from './components/Shop.jsx';
+import Cart from './components/Cart.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/home",
+        element: <Home></Home>,
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/shop",
+        element: <Shop />,
+      }, 
+      { path: '/cart', 
+        element: <Cart></Cart>
+      },
+    ],
+  },
+
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
